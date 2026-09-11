cc0415196157f18ed30092909ab6fae6e16744ea
iluvnrm <rempbacc@gmail.com>
2026-09-11 22:44:05 +0200

    Add App component with routing

---
+++ frontend/src/App.tsx
@@ -0,0 +1,35 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { LoginPage } from './pages/LoginPage'
+import { DashboardPage } from './pages/DashboardPage'
+import { GuildPage } from './pages/GuildPage'
+import { ProtectedRoute } from './components/ProtectedRoute'
+import './index.css'
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <DashboardPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/guild/:guildId"
+          element={
+            <ProtectedRoute>
+              <GuildPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
